158ca1ca0a1838ce93c345c0b053818d34862063
Joshua Blair <joshuawblair@gmail.com>
2017-08-29 22:06:55 -0400

    added simple camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private Vector3 velocity;
+	[SerializeField] private GameObject player;
+	[SerializeField] private float smoothTimeY = 0.3f;
+	[SerializeField] private float smoothTimeX = 0.3f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+		float posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY) + .05f;
+
+		transform.position = new Vector3 (posX, posY, transform.position.z);
+	}
+}
