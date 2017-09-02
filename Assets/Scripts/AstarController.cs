73a31c1e06742cfd661dff7a54e33ee4fcbb1ef2
Taylor Conners <tconners@uncc.edu>
2017-09-02 15:31:01 -0400

    new file:   Assets/Prefabs/node_test.prefab 	modified:   Assets/Scenes/Dev_worldTest.unity 	new file:   Assets/Scripts/AstarController.cs 	new file:   Assets/Scripts/AstarNode.cs 	new file:   Assets/Sprites/hollow_knight_Sprites.png 	new file:   ScalingOptions.txt

---
+++ Assets/Scripts/AstarController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstarController : MonoBehaviour {
+	[SerializeField] private SpriteRenderer room;
+	[SerializeField] private Vector3 bounds;
+	[SerializeField] private Vector3[] minMax;
+	[SerializeField] private Vector3[][] nodeLocations;
+	[SerializeField] private float length, height, minNodeDistanceX, minNodeDistanceY;
+	public GameObject node;
+
+	// Use this for initialization
+	void Awake(){
+		minMax = SpriteCoordinatesLocalToWorld (room);
+		length = minMax [1].x - minMax [0].x;
+		height = minMax [1].y - minMax [0].y;
+		minNodeDistanceX = nodePlacementIncrement (length);
+		minNodeDistanceY = nodePlacementIncrement (height);
+
+		//bounds = new Vector3 (room.bounds.extents.x, room.bounds.extents.y, room.bounds.extents.z);
+		//for (int i = Mathf.CeilToInt (minMax [0].x); i < Mathf.CeilToInt (minMax [1].x); i++)
+			//for (int j = Mathf.CeilToInt (minMax [0].y); i < Mathf.CeilToInt (minMax [1].y); i++)
+				//GameObject.Instantiate (node, new Vector3 (i, j, 0.1f), Quaternion.identity);
+
+		//GameObject min = GameObject.Instantiate (node, minMax [0], Quaternion.identity);
+		//GameObject max = GameObject.Instantiate (node, minMax [1], Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static Vector3[] SpriteCoordinatesLocalToWorld(SpriteRenderer sp){
+		//Vector3 pos = sp.transform.position;
+		Vector3[] array = new Vector3[2];
+		//array [0] = pos + sp.bounds.min;
+		//array [1] = pos + sp.bounds.max;
+		array[0] = sp.bounds.min;
+		array [1] = sp.bounds.max;
+		return array;
+	}
+
+	public static float nodePlacementIncrement(float n){
+		float min = .5f;
+		int tries = 0;
+		while (n % min != 0 && tries < 5) {
+			min += .1f;
+			tries++;
+		}
+		if (tries == 5)
+			min = .5f;
+		return min;
+	}
+}
---
+++ Assets/Scripts/AstarNode.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstarNode {
+	private int row, col, f, g, h, type;
+	private Vector3 location;
+	private bool start, goal, onPath, visited, inOpenList, inClosedList;
+	private AstarNode parent;
+	[SerializeField] private List<AstarNode> neighbors = new List<AstarNode> ();
+
+	public void reset() {
+		start = goal = onPath = visited = inOpenList = inClosedList = false;
+		parent = null;
+	}
+
+	public void setParameters(int r, int c, int t) {
+		row = r;
+		col = c;
+		type = t;
+	}
+
+	public void setF() {
+		f = g + h;
+	}
+
+	public void setG(int val) {
+		g = val;
+	}
+
+	public void setH(int val) {
+		h = val;
+	}
+
+	public void setParent (AstarNode n) {
+		parent = n;
+	}
+
+	public void setStart (bool s) {
+		start = s;
+	}
+
+	public void setGoal (bool g) {
+		goal = g;
+	}
+
+	public void setVisited (bool v) {
+		visited = v;
+	}
+
+	public void setOnPath (bool p) {
+		onPath = p;
+	}
+
+	public void setInOpenList (bool b) {
+		inOpenList = b;
+	}
+
+	public void setInClosedList (bool b) {
+		inClosedList = b;
+	}
+
+	public void setLoaction (Vector3 loc) {
+		location = loc;
+	}
+
+	public bool getInClosedList(){
+		return inClosedList;
+	}
+
+	public bool getInOpenList(){
+		return inOpenList;
+	}
+
+	public int getF(){
+		return f;
+	}
+
+	public int getH(){
+		return h;
+	}
+
+	public int getG(){
+		return g;
+	}
+
+	public int getRow(){
+		return row;
+	}
+
+	public int getCol(){
+		return col;
+	}
+
+	public int getType(){
+		return type;
+	}
+
+	public AstarNode getParent(){
+		return parent;
+	}
+
+	public List<AstarNode> getList(){
+		return neighbors;
+	}
+
+	public bool getGoal(){
+		return goal;
+	}
+
+	public string toString(){
+		return "Node: " + row + "_" + col;
+	}
+}
