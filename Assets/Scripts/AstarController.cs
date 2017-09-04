fd5c1f9391ad416040768e4d13fe577cd2b48c2e
Taylor Conners <tconners@uncc.edu>
2017-09-04 13:54:17 -0400

    Astar iteration

--- Assets/Scripts/AstarController.cs
+++ Assets/Scripts/AstarController.cs
@@ -4,27 +4,64 @@ using UnityEngine;
 
 public class AstarController : MonoBehaviour {
 	[SerializeField] private SpriteRenderer room;
-	[SerializeField] private Vector3 bounds;
 	[SerializeField] private Vector3[] minMax;
-	[SerializeField] private Vector3[][] nodeLocations;
-	[SerializeField] private float length, height, minNodeDistanceX, minNodeDistanceY;
+	[SerializeField] private Vector3[,] nodeGrid;
+	[SerializeField] private int length, height, minNodeDistance;
+	[SerializeField] private List<Vector3> nodeLocations = new List<Vector3>();
+	private float xInc, yInc;
 	public GameObject node;
 
 	// Use this for initialization
-	void Awake(){
+	void Start(){
+		minNodeDistance = 2;
 		minMax = SpriteCoordinatesLocalToWorld (room);
-		length = minMax [1].x - minMax [0].x;
-		height = minMax [1].y - minMax [0].y;
-		minNodeDistanceX = nodePlacementIncrement (length);
-		minNodeDistanceY = nodePlacementIncrement (height);
+		length = getEvenInt(Mathf.FloorToInt (minMax [1].x - minMax [0].x)/minNodeDistance);
+		height = getEvenInt(Mathf.FloorToInt(minMax [1].y - minMax [0].y)/minNodeDistance);
+		nodeGrid = new Vector3[length,height];
 
-		//bounds = new Vector3 (room.bounds.extents.x, room.bounds.extents.y, room.bounds.extents.z);
-		//for (int i = Mathf.CeilToInt (minMax [0].x); i < Mathf.CeilToInt (minMax [1].x); i++)
-			//for (int j = Mathf.CeilToInt (minMax [0].y); i < Mathf.CeilToInt (minMax [1].y); i++)
-				//GameObject.Instantiate (node, new Vector3 (i, j, 0.1f), Quaternion.identity);
+		Vector3 min, max;
+		min = minMax [0];
+		max = minMax [1];
 
-		//GameObject min = GameObject.Instantiate (node, minMax [0], Quaternion.identity);
-		//GameObject max = GameObject.Instantiate (node, minMax [1], Quaternion.identity);
+		//Debug.Log ("Length, Height: " + length + ", " + height);
+		//Debug.Log ((max.x - min.x) + ", " + (max.y - min.y));
+
+		int i = 0;
+		int j = 0;
+		//populate a list with vector3 locations for node placement
+
+		while (min.x < max.x -minNodeDistance) {
+
+			//Debug.Log ("min.x, max.x: " + min.x + ", " + max.x);
+			while (min.y < max.y - minNodeDistance) {
+				
+				//Debug.Log ("min.y, max.y: " + min.y + ", " + max.y);
+
+				nodeGrid [i, j] = new Vector3 (min.x + 1.25f, min.y + 1.25f, 0f);
+				//Debug.Log (j);
+				j++;
+				min.y += minNodeDistance;
+
+			}
+			min.x += minNodeDistance;
+			min.y = minMax [0].y;
+			//Debug.Log (i);
+			i++;
+			j = 0;
+		}
+
+
+		for (int k = 0; k < nodeGrid.GetLength (0); k++) {
+			for (int h = 0; h < nodeGrid.GetLength (1); h++) {
+				if (nodeGrid [k, h].x == 0 && nodeGrid [k, h].y == 0) {
+					//Debug.Log (k + ", " + h);
+					continue;
+				}
+				GameObject.Instantiate (node, nodeGrid [k, h], Quaternion.identity);
+			}
+		}
+			
+
 	}
 	
 	// Update is called once per frame
@@ -34,11 +71,30 @@ public class AstarController : MonoBehaviour {
 
 	public static Vector3[] SpriteCoordinatesLocalToWorld(SpriteRenderer sp){
 		//Vector3 pos = sp.transform.position;
-		Vector3[] array = new Vector3[2];
 		//array [0] = pos + sp.bounds.min;
 		//array [1] = pos + sp.bounds.max;
+		Vector3[] array = new Vector3[2];
 		array[0] = sp.bounds.min;
 		array [1] = sp.bounds.max;
+		array [0] = new Vector3 (Mathf.Ceil (array [0].x), Mathf.Floor (array [0].y), 0f);
+		array[1] = new Vector3 (Mathf.Floor(array[1].x), Mathf.Floor(array[1].y), 0f);
+
+		if (array [0].x % 2 != 0) {
+			array [0].x++;
+		}
+		if (array [0].y % 2 != 0) {
+			array [0].y++;
+		}
+		if (array [1].x % 2 != 0) {
+			array [1].x--;
+		}
+		if (array [1].y % 2 != 0) {
+			array [1].y--;
+		}
+
+		Debug.Log ("MIN : " + array [0].x + ", " + array [0].y);
+		Debug.Log ("MAX : " + array [1].x + ", " + array [1].y);
+
 		return array;
 	}
 
@@ -53,4 +109,13 @@ public class AstarController : MonoBehaviour {
 			min = .5f;
 		return min;
 	}
+
+	public static int getEvenInt(int n){
+		if (n % 2 != 0) {
+			n--;
+		}
+
+		return n;
+	}
+		
 }
