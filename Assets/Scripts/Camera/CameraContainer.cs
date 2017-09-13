ce28efdb328bfaee1c4c5a37d446bc0a16dac25c
Joshua Blair <joshuawblair@gmail.com>
2017-09-13 01:52:20 -0400

    reworked camera bounding to not use colliders, just need to implement movement bounding

--- Assets/Scripts/Camera/CameraContainer.cs
+++ Assets/Scripts/Camera/CameraContainer.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class CameraContainer : MonoBehaviour {
 
 	private Vector3 velocity;
-	[SerializeField] private GameObject player;
-	[SerializeField] private Camera camera;
 	private CameraController cameraCont;
 	private PlayerController playerCont;
-	[SerializeField] private float smoothTimeY = 0.2f;
-	[SerializeField] private float smoothTimeX = 0.2f;
 	private float posX;
 	private float posY;
-	[SerializeField] private Collider2D boundColliderBL;
-	[SerializeField] private Collider2D boundColliderTL;
-	[SerializeField] private Collider2D boundColliderBR;
-	[SerializeField] private Collider2D boundColliderTR;
+	[SerializeField] private GameObject player;
+	[SerializeField] private Camera camera;	
+	[SerializeField] private float smoothTimeY = 0.2f;
+	[SerializeField] private float smoothTimeX = 0.2f;
+	[SerializeField] private GameObject LeftWall;
+	[SerializeField] private GameObject RightWall;
+	[SerializeField] private GameObject TopWall;
+	[SerializeField] private GameObject BottomWall;
+
+	[SerializeField] private Vector3 boundBL;
+	[SerializeField] private Vector3 boundTL; 
+	[SerializeField] private Vector3 boundBR;
+	[SerializeField] private Vector3 boundTR;
+	
+	// [SerializeField] private Collider2D boundColliderBL;
+	// [SerializeField] private Collider2D boundColliderTL;
+	// [SerializeField] private Collider2D boundColliderBR;
+	// [SerializeField] private Collider2D boundColliderTR;
 
 
 	void Awake(){
@@ -29,12 +39,13 @@ public class CameraContainer : MonoBehaviour {
 		cameraCont = camera.GetComponent<CameraController>();
 	}
 
-	void Start(){		
-		boundColliderBL.transform.position = cameraCont.bottomLeft;
-		boundColliderTL.transform.position  = cameraCont.topLeft;
-		boundColliderBR.transform.position  = cameraCont.bottomRight;
-		boundColliderTR.transform.position  = cameraCont.topRight;
-	}
+	 void Start(){		
+	// 	boundColliderBL.transform.position = cameraCont.bottomLeft;
+	// 	boundColliderTL.transform.position  = cameraCont.topLeft;
+	// 	boundColliderBR.transform.position  = cameraCont.bottomRight;
+	// 	boundColliderTR.transform.position  = cameraCont.topRight;
+		getRoomCorners();
+	 }
 	
 	void FixedUpdate () {
 		
@@ -42,23 +53,27 @@ public class CameraContainer : MonoBehaviour {
 		posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY) + .05f;
 		transform.position = new Vector3(posX, posY, 0);
 
-		boundColliderBL.transform.position = cameraCont.bottomLeft;
-		boundColliderTL.transform.position  = cameraCont.topLeft;
-		boundColliderBR.transform.position  = cameraCont.bottomRight;
-		boundColliderTR.transform.position  = cameraCont.topRight;
+
+		// boundColliderBL.transform.position = cameraCont.bottomLeft;
+		// boundColliderTL.transform.position  = cameraCont.topLeft;
+		// boundColliderBR.transform.position  = cameraCont.bottomRight;
+		// boundColliderTR.transform.position  = cameraCont.topRight;
 
 	}
 
-	void OnCollisionEnter2D(Collision2D other){
-		Debug.Log ("hit");
-//			if (other.gameObject.tag != "CameraBounds") {
-//				Physics2D.IgnoreCollision (other.collider, boundColliderBL);
-//				Physics2D.IgnoreCollision (other.collider, boundColliderTL);
-//				Physics2D.IgnoreCollision (other.collider, boundColliderBR);
-//				Physics2D.IgnoreCollision (other.collider, boundColliderTR);
-//			}
+	void OnDrawGizmosSelected(){
+		Gizmos.color = Color.green;
+		Gizmos.DrawSphere (boundBL, .5f);
+		Gizmos.DrawSphere (boundTL, .5f);
+		Gizmos.DrawSphere (boundBR, .5f);
+		Gizmos.DrawSphere (boundTR, .5f);
+	}
 
-
+	void getRoomCorners(){
+		boundBL = new Vector3((BottomWall.transform.position.x - (BottomWall.transform.localScale.x)/2), (LeftWall.transform.position.y-(LeftWall.transform.localScale.x)/2), 0);
+		boundTL = new Vector3((TopWall.transform.position.x - (BottomWall.transform.localScale.x)/2), (LeftWall.transform.position.y+(LeftWall.transform.localScale.x)/2), 0);
+		boundBR = new Vector3((BottomWall.transform.position.x + (BottomWall.transform.localScale.x)/2), (RightWall.transform.position.y-(RightWall.transform.localScale.x)/2), 0);
+		boundTR = new Vector3((TopWall.transform.position.x + (TopWall.transform.localScale.x)/2), (RightWall.transform.position.y+(RightWall.transform.localScale.x)/2), 0);
 	}
 
 }
