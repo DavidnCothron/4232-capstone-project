2dfd4c306f4e6c378c97d0b610d4d9fca844bcf5
Taylor Conners <tconners@uncc.edu>
2017-09-13 10:58:52 -0400

    Progress on AStar

--- Assets/Scripts/Pathfinding/AstarController.cs
+++ Assets/Scripts/Pathfinding/AstarController.cs
@@ -4,48 +4,49 @@ using UnityEngine;
 
 public class AstarController : MonoBehaviour {
 	[SerializeField] private SpriteRenderer room;
-	[SerializeField] private Vector3[] minMax;
-	[SerializeField] private Vector3[,] nodeGrid;
-	[SerializeField] private int length, height, minNodeDistance;
-	[SerializeField] private List<Vector3> nodeLocations = new List<Vector3>();
+	private Vector3[] minMax;
+	private Vector3[,] nodeGrid;
+	private AstarNode[,] nodes;
+	private int length, height, minNodeDistance;
 	private float xInc, yInc;
 	public GameObject node;
+	private bool hitsNotNull, hitsBoundaries;
+
+	private bool hitsNotAstarTest; // temporary condition for testing algorithm
 
 	// Use this for initialization
 	void Start(){
 		minNodeDistance = 2;
 		minMax = SpriteCoordinatesLocalToWorld (room);
-		length = getEvenInt(Mathf.FloorToInt (minMax [1].x - minMax [0].x)/minNodeDistance);
-		height = getEvenInt(Mathf.FloorToInt(minMax [1].y - minMax [0].y)/minNodeDistance);
+		//Debug.Log ((minMax [1].x - minMax [0].x));
+
+		length = (int)((minMax [1].x - minMax [0].x)/minNodeDistance);//getEvenInt(Mathf.FloorToInt (minMax [1].x - minMax [0].x))
+		height = (int)((minMax [1].y - minMax [0].y)/minNodeDistance); //divided by minNodeDistance
 		nodeGrid = new Vector3[length,height];
+		nodes = new AstarNode[length, height];
 
+		//For traversal
 		Vector3 min, max;
-		min = minMax [0];
+		min = minMax [0]; //new vector3 with rounded floats like length/height
 		max = minMax [1];
-
-		//Debug.Log ("Length, Height: " + length + ", " + height);
-		//Debug.Log ((max.x - min.x) + ", " + (max.y - min.y));
 
 		int i = 0;
 		int j = 0;
-		//populate a list with vector3 locations for node placement
 
-		while (min.x < max.x -minNodeDistance) {
+		//Debug.Log ("Length: " + length + " height: " + height);
+		//Debug.Log ("Max.x - min.x: " + (max.x - min.x));
+		//Debug.Log ("Max.y - min.y: " + (max.y - min.y));
 
-			//Debug.Log ("min.x, max.x: " + min.x + ", " + max.x);
-			while (min.y < max.y - minNodeDistance) {
-				
-				//Debug.Log ("min.y, max.y: " + min.y + ", " + max.y);
-
+		//populate a 2d array with vector3 locations for node placement
+		while (min.x < max.x) {
+			while (min.y < max.y) {
 				nodeGrid [i, j] = new Vector3 (min.x + 1.25f, min.y + 1.25f, 0f);
-				//Debug.Log (j);
 				j++;
 				min.y += minNodeDistance;
 
 			}
 			min.x += minNodeDistance;
 			min.y = minMax [0].y;
-			//Debug.Log (i);
 			i++;
 			j = 0;
 		}
@@ -54,19 +55,46 @@ public class AstarController : MonoBehaviour {
 		for (int k = 0; k < nodeGrid.GetLength (0); k++) {
 			for (int h = 0; h < nodeGrid.GetLength (1); h++) {
 				if (nodeGrid [k, h].x == 0 && nodeGrid [k, h].y == 0) {
-					//Debug.Log (k + ", " + h);
+					Debug.Log ("NODE INSTANTIATED AT (0,0); NODE GRID TOO LARGE FOR # OF NODES");
 					continue;
 				}
-				GameObject.Instantiate (node, nodeGrid [k, h], Quaternion.identity);
+				//GameObject obj = GameObject.Instantiate (node, nodeGrid [k, h], Quaternion.identity);
+				//Debug.DrawRay(new Vector3 (nodeGrid[k,h].x, nodeGrid[k,h].y,
+					//-UnityDepth.instance.unityDepth), Vector3.forward * UnityDepth.instance.unityDepth, Color.red, 30, false);
+				Vector3 rayStart = new Vector3 (nodeGrid [k, h].x, nodeGrid [k, h].y, -UnityDepth.instance.unityDepth);
+				Vector3 direction = (Vector3.forward * (UnityDepth.instance.unityDepth));
+				RaycastHit2D hit = Physics2D.Raycast (rayStart, direction);
+
+				//Check to see if Vector at nodeGrid[k,h] is in a pathable position
+				if (hit == null || (hit != null && (hit.collider == null || hit.collider.tag == "Player"))) {
+					//If vector is in pathable position, check to see if it's in current room
+					RaycastHit2D hitUp = Physics2D.Raycast (nodeGrid [k, h], Vector3.up, 100);
+					RaycastHit2D hitDown = Physics2D.Raycast (nodeGrid [k, h], Vector3.down, 100);
+					RaycastHit2D hitRight = Physics2D.Raycast (nodeGrid [k, h], Vector3.right, 100);
+					RaycastHit2D hitLeft = Physics2D.Raycast (nodeGrid [k, h], Vector3.left, 100);
+					hitsNotNull = hitsBoundaries = false;
+					//Will have to add condition to check if the hit is in the currently occupied room
+					//Condition: Each raycast from position hits something
+					if (hitUp != null && hitDown != null && hitRight != null && hitLeft != null)
+						hitsNotNull = true;
+					//Condition: Each hit's collider is not null
+					if (hitsNotNull && (hitUp.collider != null && hitDown.collider != null && hitRight.collider != null && hitLeft.collider != null))
+						hitsBoundaries = true;
+					//Temporary Condition: hit's gameobject is not an Astar Node test object
+//					if (hitsBoundaries && (hitUp.transform.tag != "testAstar" && hitDown.transform.tag != "testAstar"
+//						&& hitRight.transform.tag != "testAstar" && hitLeft.transform.tag != "testAstar"))
+//						hitsNotAstarTest = true;
+					
+					//If the above conditions are true, the node is un the current room.
+					if (hitsNotNull && hitsBoundaries) {
+						GameObject obj = GameObject.Instantiate (node, nodeGrid [k, h], Quaternion.identity);
+						obj.tag = "testAstar";
+						obj.layer = 2;
+					}
+				}
+
 			}
 		}
-			
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
 	}
 
 	public static Vector3[] SpriteCoordinatesLocalToWorld(SpriteRenderer sp){
@@ -76,46 +104,23 @@ public class AstarController : MonoBehaviour {
 		Vector3[] array = new Vector3[2];
 		array[0] = sp.bounds.min;
 		array [1] = sp.bounds.max;
-		array [0] = new Vector3 (Mathf.Ceil (array [0].x), Mathf.Floor (array [0].y), 0f);
-		array[1] = new Vector3 (Mathf.Floor(array[1].x), Mathf.Floor(array[1].y), 0f);
+		array [0] = new Vector3 (Mathf.Floor (array [0].x), Mathf.Floor (array [0].y), 0f);
+		array[1] = new Vector3 (Mathf.Ceil(array[1].x), Mathf.Ceil(array[1].y), 0f);
 
-		if (array [0].x % 2 != 0) {
-			array [0].x++;
-		}
-		if (array [0].y % 2 != 0) {
-			array [0].y++;
-		}
-		if (array [1].x % 2 != 0) {
-			array [1].x--;
-		}
-		if (array [1].y % 2 != 0) {
-			array [1].y--;
-		}
-
-		Debug.Log ("MIN : " + array [0].x + ", " + array [0].y);
-		Debug.Log ("MAX : " + array [1].x + ", " + array [1].y);
+		//check to ensure even numbers on vector floats. reduce using turnary statement
+		if (array [0].x % 2 != 0) 
+			array [0].x--;
+		
+		if (array [0].y % 2 != 0) 
+			array [0].y--;
+		
+		if (array [1].x % 2 != 0) 
+			array [1].x++;
+		
+		if (array [1].y % 2 != 0) 
+			array [1].y++;
+		
 
 		return array;
-	}
-
-	public static float nodePlacementIncrement(float n){
-		float min = .5f;
-		int tries = 0;
-		while (n % min != 0 && tries < 5) {
-			min += .1f;
-			tries++;
-		}
-		if (tries == 5)
-			min = .5f;
-		return min;
-	}
-
-	public static int getEvenInt(int n){
-		if (n % 2 != 0) {
-			n--;
-		}
-
-		return n;
-	}
-		
+	}		
 }
