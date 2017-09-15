e84ad9b1c58af3b676e3e96751caf6af3572bd89
Joshua Blair <joshua.blair@csi.corp>
2017-09-14 23:07:54 -0400

    camera bounding workinggit status yay!

--- Assets/Scripts/Camera/CameraContainer.cs
+++ Assets/Scripts/Camera/CameraContainer.cs
@@ -13,21 +13,15 @@ public class CameraContainer : MonoBehaviour {
 	[SerializeField] private Camera camera;	
 	[SerializeField] private float smoothTimeY = 0.2f;
 	[SerializeField] private float smoothTimeX = 0.2f;
+	//walls of the room, set in inspector
 	[SerializeField] private GameObject LeftWall;
 	[SerializeField] private GameObject RightWall;
 	[SerializeField] private GameObject TopWall;
 	[SerializeField] private GameObject BottomWall;
 
+	//
 	[SerializeField] private Vector3 boundBL;
-	[SerializeField] private Vector3 boundTL; 
-	[SerializeField] private Vector3 boundBR;
 	[SerializeField] private Vector3 boundTR;
-	
-	// [SerializeField] private Collider2D boundColliderBL;
-	// [SerializeField] private Collider2D boundColliderTL;
-	// [SerializeField] private Collider2D boundColliderBR;
-	// [SerializeField] private Collider2D boundColliderTR;
-
 
 	void Awake(){
 		UnityDepth.instance.FindUnityDepth ();
@@ -39,40 +33,24 @@ public class CameraContainer : MonoBehaviour {
 		cameraCont = camera.GetComponent<CameraController>();
 	}
 
-	 void Start(){		
-	// 	boundColliderBL.transform.position = cameraCont.bottomLeft;
-	// 	boundColliderTL.transform.position  = cameraCont.topLeft;
-	// 	boundColliderBR.transform.position  = cameraCont.bottomRight;
-	// 	boundColliderTR.transform.position  = cameraCont.topRight;
+	 void Start(){
 		getRoomCorners();
 	 }
 	
 	void FixedUpdate () {
-		
 		posX = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
 		posY = Mathf.SmoothDamp (transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY) + .05f;
-		transform.position = new Vector3(posX, posY, 0);
+		transform.position = new Vector3(
+	/*x*/	Mathf.Clamp(posX, boundBL.x + Mathf.Abs(this.transform.position.x - cameraCont.bottomLeft.x), boundTR.x - Mathf.Abs(this.transform.position.x - cameraCont.topRight.x)),
+	/*y*/	Mathf.Clamp(posY, boundBL.y + Mathf.Abs(this.transform.position.y - cameraCont.bottomLeft.y), boundTR.y - Mathf.Abs(this.transform.position.y - cameraCont.topRight.y)),
+	/*z*/	0f
+		);
 
 
-		// boundColliderBL.transform.position = cameraCont.bottomLeft;
-		// boundColliderTL.transform.position  = cameraCont.topLeft;
-		// boundColliderBR.transform.position  = cameraCont.bottomRight;
-		// boundColliderTR.transform.position  = cameraCont.topRight;
-
-	}
-
-	void OnDrawGizmosSelected(){
-		Gizmos.color = Color.green;
-		Gizmos.DrawSphere (boundBL, .5f);
-		Gizmos.DrawSphere (boundTL, .5f);
-		Gizmos.DrawSphere (boundBR, .5f);
-		Gizmos.DrawSphere (boundTR, .5f);
 	}
 
 	void getRoomCorners(){
 		boundBL = new Vector3((BottomWall.transform.position.x - (BottomWall.transform.localScale.x)/2), (LeftWall.transform.position.y-(LeftWall.transform.localScale.x)/2), 0);
-		boundTL = new Vector3((TopWall.transform.position.x - (BottomWall.transform.localScale.x)/2), (LeftWall.transform.position.y+(LeftWall.transform.localScale.x)/2), 0);
-		boundBR = new Vector3((BottomWall.transform.position.x + (BottomWall.transform.localScale.x)/2), (RightWall.transform.position.y-(RightWall.transform.localScale.x)/2), 0);
 		boundTR = new Vector3((TopWall.transform.position.x + (TopWall.transform.localScale.x)/2), (RightWall.transform.position.y+(RightWall.transform.localScale.x)/2), 0);
 	}
 
--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -19,14 +19,7 @@ public class CameraController : MonoBehaviour {
 		camera = Camera.FindObjectOfType (typeof(Camera)) as Camera;
 	}
 
-	void Start(){
-		topRight = camera.ViewportToWorldPoint (new Vector3 (1, 1, 16.875f));
-		bottomRight = camera.ViewportToWorldPoint (new Vector3 (1, 0, 16.875f));
-		topLeft = camera.ViewportToWorldPoint (new Vector3 (0, 1, 16.875f));
-		bottomLeft = camera.ViewportToWorldPoint (new Vector3 (0, 0, 16.875f));
-	}
 
-	// Update is called once per frame
 	void FixedUpdate () {
 		topRight = camera.ViewportToWorldPoint (new Vector3 (1, 1, 16.875f));
 		bottomRight = camera.ViewportToWorldPoint (new Vector3 (1, 0, 16.875f));
@@ -42,53 +35,4 @@ public class CameraController : MonoBehaviour {
 		Gizmos.DrawSphere (bottomLeft, .5f);
 	}
 
-	
-	/*/
-	void Update()
-	{
-		Camera.main.camera.
-		topRightEdgeScreen = Camera.main.WorldToScreenPoint(topRightEdge);
-		downLeftEdgeScreen = Camera.main.WorldToScreenPoint(downLeftEdge);
-
-		Debug.Log(downLeftEdgeScreen + "  " + Screen.height);
-	
-		// Is the camera out of the map bounds?
-		if(topRightEdgeScreen.x < Screen.width || topRightEdgeScreen.y < Screen.height || downLeftEdgeScreen.x > 0 || downLeftEdgeScreen.y>0){
-			//smack a big plane at the camera position that covers more than the screen is showing
-			cameraPositionFixPlane = new Plane(Vector3.forward*10, Camera.main.transform.position);
-
-			//move the top right edge back so its inside the screen again
-			Vector3 topRightEdgeScreenFixed = Camera.main.ScreenToWorldPoint(new Vector3(Mathf.Max(Screen.width, topRightEdgeScreen.x), Mathf.Max(Screen.height,topRightEdgeScreen.y), topRightEdgeScreen.z));
-			//now we know the offset the camera should move at distance z to fix the top right edge
-			Vector3 topRightOffsetAtDistance = topRightEdgeScreenFixed-topRightEdge;
-
-			//this time for the down left edge
-			Vector3 downLeftEdgeScreenFixed = Camera.main.ScreenToWorldPoint(new Vector3(Mathf.Min(0, downLeftEdgeScreen.x), Mathf.Min(0, downLeftEdgeScreen.y), downLeftEdgeScreen.z));
-			//now we know the offset the camera should move at distance z to fix the down left edge
-			Vector3 downLeftOffsetAtDistance = downLeftEdgeScreenFixed-downLeftEdge;
-
-			Debug.Log ("offset: "+downLeftOffsetAtDistance);
-
-
-			//where is the center of the screen translated at given distance
-			Vector3 cameraCenterAtDistance = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width/2.0f, Screen.height/2.0f, topRightEdge.z));
-			//now lets offset the center of the screen with the offset we found
-			Vector3 cameraCenterAtDistanceFixed = new Vector3(cameraCenterAtDistance.x-topRightOffsetAtDistance.x-downLeftOffsetAtDistance.x, cameraCenterAtDistance.y-topRightOffsetAtDistance.y-downLeftOffsetAtDistance.y, cameraCenterAtDistance.z);
-
-			//here we generate a ray at the camera center at distance pointing back to the camera
-			Ray rayFromFixedDistanceToCameraPlane = new Ray(cameraCenterAtDistanceFixed, -Camera.main.transform.forward);
-
-			//this is where the magic happens, lets raycast back to the plane i smacked infront of the  camera
-			float d;
-			cameraPositionFixPlane.Raycast(rayFromFixedDistanceToCameraPlane, out d);
-
-			//where did the raycast hit the camera plane?
-			Vector3 planeHitPoint = rayFromFixedDistanceToCameraPlane.GetPoint(d);
-
-			//position camera at the hitpoint we found
-			Camera.main.transform.position = new Vector3(planeHitPoint.x, planeHitPoint.y, Camera.main.transform.position.z);
-
-	}*/
-	
-	
 }
