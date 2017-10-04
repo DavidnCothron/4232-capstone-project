71c7c555f7b9c87c4ee3e45426ccbe2b3f0ff97c
Joshua Blair <joshua.blair@csi.corp>
2017-10-04 15:39:17 -0400

    moved doors in test scenes

--- Assets/Scripts/World/AreaControl.cs
+++ Assets/Scripts/World/AreaControl.cs
@@ -7,20 +7,21 @@ public class AreaControl : MonoBehaviour {
 
 
 	private int areaEntry;
-	private Door roomToEnter;
-	
+	private Door roomToEnter;	
 	private PlayerManager player;
 
 	
 	void Start(){
 		Debug.Log("test on start");
 		areaEntry = GameControl.control.GetAreaEntryID();
+		player = GameObject.Find ("Player").GetComponent(typeof(PlayerManager)) as PlayerManager; //gets player reference
 		if(areaEntry == 1){
-			roomToEnter = GameObject.Find("Room1").GetComponentInChildren<Door>();//gets room to enter based on value passed from GetAreaEntryId in Game control
-			player = GameObject.Find ("Player").GetComponent(typeof(PlayerManager)) as PlayerManager; //gets player reference
-			StartCoroutine(roomToEnter.areaTransitionIn(player.GetComponent<Collider2D>()));//start area transition in cooroutine
+			roomToEnter = GameObject.Find("EntryRoom1").GetComponentInChildren<Door>();//gets room to enter based on value passed from GetAreaEntryId in Game control			
 		}
+		StartCoroutine(roomToEnter.areaTransitionIn(player.GetComponent<Collider2D>()));//start area transition in cooroutine
 	}
+
+
 	// Update is called once per frame
 	void Update () {
 		
