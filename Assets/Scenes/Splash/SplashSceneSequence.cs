ed3e6922f4ff0f7d17012a3e3cf9928963df7155
DavidnCothron <davidncothron@gmail.com>
2017-11-27 16:17:04 -0500

    Added new player sprite assets to create player sprite sheet from

---
+++ Assets/Scenes/Splash/SplashSceneSequence.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashSceneSequence : MonoBehaviour {
+
+	public static int SceneNumber;
+	// Use this for initialization
+	void Start () {
+		if(SceneNumber == 0) {
+			StartCoroutine(ToSplashTwo());
+		}
+		else if(SceneNumber == 1){
+			StartCoroutine(ToMainMenu());
+		}
+		
+	}
+
+	IEnumerator ToSplashTwo(){
+		yield return new WaitForSeconds(5);
+		SceneNumber = 1;
+		SceneManager.LoadScene(1);
+	}
+
+	IEnumerator ToMainMenu(){
+		yield return new WaitForSeconds(5);
+		SceneNumber = 2;
+		SceneManager.LoadScene(2);
+	}
+}
