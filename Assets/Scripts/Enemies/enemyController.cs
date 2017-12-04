c175435bf1585a9cd3bc55a202e5ce1544d047bf
Joshua Blair <joshuawblair@gmail.com>
2017-12-04 18:06:03 -0500

    finished basic enemy following AI

--- Assets/Scripts/Enemies/enemyController.cs
+++ Assets/Scripts/Enemies/enemyController.cs
@@ -9,15 +9,34 @@ public class enemyController : PhysicsObject {
 
 	Rigidbody2D rigidBody;
 	Transform playerTrans;
-	Collider ownCollider;
+
+	//Animaitons
+	public float CurrentAttackTime;
+    public string standAnimationState;
+    public string RunAnimationState;
+    public string JumpAnimationState;
+    public string FallAnimationState;
+    public string Attack1AnimationState;
+    public float Attack1AnimationDuration;
+    public string Attack2AnimationState;
+    public float Attack2AnimationDuration;
+    public string Attack3AnimationState;
+    public float Attack3AnimationDuration;
+    public string DefAnimationState;
+    public string DeathAnimationState;
+	public enum state { stand, jump, fall, run, attack1, attack2, attack3, Def, Death}
+	public state State;
+
+	public bool onAlert = false;
 	public bool isBusy = false; //bool that controls actions during FixedUpdate
 	public bool isGrounded = true;
 	public float jumpTakeOffSpeed = 7f;
 	public float maxSpeed = 3.5f;
 	public bool haltInput = false;
 	public int health = 5;
-	public float sightDistance = 10f;
+	public float sightDistance = 25f;
 	public float arc = 45f;
+	Vector3 enemyFacingDirection = new Vector3(1,0,0);
 	private SpriteRenderer spriteRenderer;
 	private Animator animator;
 
@@ -26,56 +45,126 @@ public class enemyController : PhysicsObject {
 		rigidBody = gameObject.GetComponent (typeof(Rigidbody2D)) as Rigidbody2D;
 		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
 		animator = gameObject.GetComponent<Animator> ();
-		ownCollider = gameObject.GetComponent<Collider>();
 	}
 
+
+	//TODO: Add logic to path towards last known position if can no longer see target
 	protected override void ComputeVelocity () {
 		Vector2 move = Vector2.zero;
+		Vector3 scale = transform.localScale;		
+
 		if (!haltInput)
 		{
-			canSee();
-			// #region Enemy Movement
-			// Vector3 target = GameControl.control.GetPlayerTransform().position - this.transform.position;
-			// if(target.magnitude < 5){
-			// 	move = target;
-			// 	targetVelocity = move * maxSpeed;
-			// }
-			// #endregion
+			playerTrans = GameControl.control.GetPlayerTransform();
+			Vector3 target = playerTrans.position - transform.position;
+			#region Enemy Movement
+
+			//TODO: implement patrol and chase
+			if(Vector3.SqrMagnitude(target) < sightDistance){//if withing sight distance
+				if(canSee(target))//if can see target
+					onAlert = true;		//put enemy on alert
+			}else{
+				onAlert = false;//if not withing sight distance
+			}
+
+			if(onAlert){
+				//Debug.Log(Mathf.Abs(target.x));
+				if(Mathf.Abs(target.x) > .8f){//move towards target until reasonably close
+					move = target;
+					targetVelocity = move * maxSpeed;
+					State = state.run;//play run animation
+				}else{
+					State = state.stand;//play stand animation
+				}
+
+				if ((target.x + transform.position.x) < transform.position.x)//if moving left face left
+				{ //Flip to left
+					scale.x = -Mathf.Abs(scale.x);
+					transform.localScale = scale;
+					enemyFacingDirection = -transform.right;
+				}
+				else if ((target.x + transform.position.x) > transform.position.x)//if moving right face right
+				{ //Flip to right
+					scale.x = Mathf.Abs(scale.x);
+					transform.localScale = scale;
+					enemyFacingDirection = transform.right;
+				}
+			}
+			
+			if(!grounded){//if falling play falling animation
+				State = state.fall;
+			}
+			UpdateAnimations();	//update animation state
+			#endregion
+			
 		}
 	}
 
-	bool canSee(){
+	bool canSee(Vector3 targetVector){
 		playerTrans = GameControl.control.GetPlayerTransform();
-		Vector3 target =  transform.position - playerTrans.position;
-		if(Vector3.Distance(transform.position, playerTrans.position) < sightDistance){
-			//Debug.Log("ang: " + Vector3.Angle(transform.right, playerTrans.position));
-			//Debug.Log("dot: " + Vector3.Dot(transform.right, playerTrans.position));
-				if(Vector3.Dot(transform.right, playerTrans.position) > 0 && Vector3.Angle(transform.right, playerTrans.position) < arc){
-					//RaycastHit2D[] hits = new RaycastHit2D[10];
-					//Physics2D.RaycastNonAlloc(transform.position, playerTrans.position - transform.position, hits);
-					// foreach(var hit in hits){
-					// 	Debug.Log(hit.collider.tag);
-					// 	if(hit.collider.tag == "Player"){				
-					// 		Debug.Log("player in sight");
-					// 		return true;					
-					// 	}					
-					// }
-					RaycastHit2D hit = new RaycastHit2D();
-					Debug.Log(LayerMask.LayerToName(13));
-					hit = Physics2D.Raycast(transform.position, (playerTrans.position- transform.position)+ new Vector3(0, .5f, 0), sightDistance, 0);
-					Debug.DrawRay(transform.position, (playerTrans.position- transform.position)+ new Vector3(0, .5f, 0), Color.green);
-					Debug.Log(hit.collider.tag);
-					if(hit.collider.tag == "Player"){				
-							Debug.Log("player in sight");
-					 		return true;					
-					 	}
+		if(Vector3.SqrMagnitude(targetVector) < sightDistance){
+			if(Vector3.Dot(enemyFacingDirection, playerTrans.position) > 0 && Vector3.Angle(enemyFacingDirection, playerTrans.position) < arc){
 
-					
+				RaycastHit2D hit = new RaycastHit2D();
+				LayerMask mask = ~(1 << LayerMask.NameToLayer("AttackLayer") | 1 << LayerMask.NameToLayer("RoomBackground"));//ignore self (attack layer) and roombackground layer
+				
+				hit = Physics2D.Raycast(transform.position, (targetVector+ new Vector3(0, .5f, 0)), sightDistance, mask);//raycast towards player
+				Debug.DrawRay(transform.position, (targetVector + new Vector3(0, .5f, 0)), Color.green);
+				
+				if(hit.collider.tag == "Player"){//if hit player then return true. hit anything else return false.
+					return true;					
 				}
 			}
-			return false;
+		}
+		return false;
 	}
 
+ void UpdateAnimations()
+    {
+		//Debug.Log(State);
+        if (State == state.stand)
+        {
+            animator.CrossFade(standAnimationState, 0f);
+        } else
+        if (State == state.run)
+        {
+            animator.CrossFade(RunAnimationState, 0f);
+        }
+        else
+        if (State == state.jump)
+        {
+            animator.CrossFade(JumpAnimationState, 0f);
+        }
+        else if (State == state.fall)
+        {
+            animator.CrossFade(FallAnimationState, 0f);
+        }
+        else
+        if (State == state.attack1)
+        {
+            animator.CrossFade(Attack1AnimationState, 0f);
+        }
+        else
+        if (State == state.attack2)
+        {
+            animator.CrossFade(Attack2AnimationState, 0f);
+        }
+        else
+        if (State == state.attack3)
+        {
+            animator.CrossFade(Attack3AnimationState, 0f);
+        } else
+        if (State == state.Death)
+        {
+            animator.CrossFade(DeathAnimationState, 0f);
+            print("morra");
+        }
+        else if (State == state.Def)
+        {
+            animator.CrossFade(DefAnimationState, 0f);
+        }
+    }
+	/*
 	void OnCollisionStay2D(Collision2D coll)
 	{
 		rigidBody.gravityScale = 1f;
@@ -85,8 +174,10 @@ public class enemyController : PhysicsObject {
 
 	void OnCollisionExit2D(Collision2D coll)
 	{
-		if (coll.gameObject.tag == "ground")
+		if (coll.gameObject.tag == "ground"){
 			isGrounded = false;
-	}
+			State = state.fall;
+		}
+	}*/
 
 }
