d387e16926766b2ca5cd4d4c33d2b2b3fc644043
Unknown <davidncothron@gmail.com>
2017-12-10 00:09:50 -0500

    Added Evil Eye Boss Mechanic framework
    Also added artwork for Evil Eye, new boss room, and moved the player to the boss room for testing.

---
+++ Assets/EvilEyeLidScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilEyeLidScript : MonoBehaviour {
+
+	public Animator eyeLidAnimator;
+	public bool isDead = false;
+	public int health = 5;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		eyeLidAnimator.SetBool("isDead", isDead);
+
+		if(health <= 0){
+			isDead = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Enemies/EvilEyeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilEyeScript : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject particleSystem;
+	public Transform playerPosition;
+	public Animator eyeAnimator;
+	public bool isActive = false;
+	//public bool isDead = false;
+	//public int health = 5;
+
+	//public Animator eyeLidAnimator;
+
+	void Awake () {
+		particleSystem.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		eyeAnimator.SetBool("looking", isActive);
+		if(isActive){
+			transform.right = (Vector2)(playerPosition.position - transform.position);
+		}
+	}
+}
