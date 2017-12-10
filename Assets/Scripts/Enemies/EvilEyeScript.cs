522336f30e73f5282a2f515e45956a66b79650aa
Unknown <davidncothron@gmail.com>
2017-12-10 00:31:44 -0500

    Added particle effects and "attacking to Evil Eye

--- Assets/Scripts/Enemies/EvilEyeScript.cs
+++ Assets/Scripts/Enemies/EvilEyeScript.cs
@@ -9,6 +9,10 @@ public class EvilEyeScript : MonoBehaviour {
 	public Transform playerPosition;
 	public Animator eyeAnimator;
 	public bool isActive = false;
+	public float attackCooldown;
+	public float attackCooldownMax = 2f;
+	public float attackChargeUp;
+	public float attackChargeUpMax = 1f;
 	//public bool isDead = false;
 	//public int health = 5;
 
@@ -16,6 +20,8 @@ public class EvilEyeScript : MonoBehaviour {
 
 	void Awake () {
 		particleSystem.SetActive(false);
+		attackChargeUp = attackChargeUpMax;
+		attackCooldown = attackCooldownMax;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +29,23 @@ public class EvilEyeScript : MonoBehaviour {
 		eyeAnimator.SetBool("looking", isActive);
 		if(isActive){
 			transform.right = (Vector2)(playerPosition.position - transform.position);
+			attackCooldown -= Time.deltaTime;
+			if(attackCooldown <= 0f){
+				if(!particleSystem.active)
+					particleSystem.SetActive(true);
+				attackChargeUp -= Time.deltaTime;
+				if(attackChargeUp <= 0){
+					attackCooldown = attackCooldownMax;
+					attackChargeUp = attackChargeUpMax;
+					FireEyeBall();
+					particleSystem.SetActive(false);
+				}
+			}
 		}
+	}
+
+	void FireEyeBall(){
+		Debug.Log("Die, player!");
+		return;
 	}
 }
