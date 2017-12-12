7131d688e934b4821f6e1fbc34a2de0d0897b124
DavidnCothron <davidncothron@gmail.com>
2017-12-11 19:41:52 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/BossDoorScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDoorScript : MonoBehaviour {
+
+	//public BossKeyScript bossKey;
+	public bool isUnlocked = false;
+	void Awake () {
+		isUnlocked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player"){
+			
+		}
+	}
+}
