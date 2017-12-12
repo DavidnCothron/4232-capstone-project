ec16625ef4a752af2184cbce097e55d0fdfc4637
Taylor Conners <tconners@uncc.edu>
2017-12-12 00:03:30 -0500

    Added new assets, light flickering effect, staggered play of torch sounds so they're not all in unison

---
+++ Assets/Scripts/World/Objects/LightFlicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+	private Light thisLight;
+	private Color startColor;
+	private float timeGone, intensity, randomMult, randomAdd, randomFrequency, range;
+	private float changeValue;
+	// Use this for initialization
+	void OnEnable () {
+		randomize();
+		thisLight = GetComponent<Light>();
+		if (thisLight != null) {
+			startColor = thisLight.color;
+			intensity = thisLight.intensity;
+			range = thisLight.range;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeGone = Time.time/2;
+		timeGone = timeGone - Mathf.Floor(timeGone);
+		thisLight.color = startColor * changeCalc();
+		thisLight.intensity = intensity * changeCalc();
+		thisLight.range = range * changeCalc();
+	}
+
+	float changeCalc(){
+		changeValue = -Mathf.Sin(timeGone * randomFrequency * Mathf.PI) * randomMult + randomAdd;
+		return changeValue;
+	}
+
+	void randomize(){
+		randomAdd = Random.Range(55,95)/100f;
+		randomMult = Random.Range(5,10)/100f;
+		randomFrequency = Random.Range(5, 12);
+	}
+}
---
+++ Assets/Scripts/World/Objects/RandomAudioStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAudioStart : MonoBehaviour {
+	private AudioSource audioSource;
+	private float waitTime;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		waitTime = Random.Range(0,3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		waitTime -= Time.deltaTime;
+		if (waitTime <= 0) {
+			audioSource.Play();
+			this.GetComponent<RandomAudioStart>().enabled = false;
+		}
+	}
+}
