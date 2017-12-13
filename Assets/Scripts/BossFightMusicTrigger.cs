b5cb0d92ed4c343abb38584e8d8b0e9a5e0f14b8
Unknown <davidncothron@gmail.com>
2017-12-13 03:42:16 -0600

    Finished boss logic and added boss area trigger

--- Assets/Scripts/BossFightMusicTrigger.cs
+++ Assets/Scripts/BossFightMusicTrigger.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class BossFightMusicTrigger : MonoBehaviour {
 	[SerializeField] private AudioController audioController;
+	public MouthOfEvilController mouthOfEvil;
 	// Use this for initialization
 	void OnTriggerEnter2D(Collider2D c){
-		if (c.CompareTag("Player")) audioController.setFightBoss(true);
+		if (c.CompareTag("Player")) {
+			audioController.setFightBoss(true);
+			mouthOfEvil.isBossFightActive = true;
+			mouthOfEvil.startBossFight = true;
+		}
 	}
 }
--- Assets/Scripts/Enemies/EvilEyeScript.cs
+++ Assets/Scripts/Enemies/EvilEyeScript.cs
@@ -6,13 +6,16 @@ public class EvilEyeScript : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject particleSystem;
+	public SpriteRenderer spriteFlash;
 	public Transform playerPosition;
 	public Animator eyeAnimator;
+	public EvilEyeLidScript eyeLid;
 	public bool isActive;
 	public float attackCooldown;
 	public float attackCooldownMax = 2f;
 	public float attackChargeUp;
 	public float attackChargeUpMax = 1f;
+	public int eyeHealth;
 	//public bool isDead = false;
 	//public int health = 5;
 
@@ -29,6 +32,11 @@ public class EvilEyeScript : MonoBehaviour {
 	void Update () {
 		eyeAnimator.SetBool("looking", isActive);
 		if(isActive){
+			if(eyeHealth <= 0){
+				isActive = false;
+				eyeLid.isDead = true;
+				return;
+			}
 			transform.right = (Vector2)(playerPosition.position - transform.position);
 			attackCooldown -= Time.deltaTime;
 			if(attackCooldown <= 0f){
@@ -45,8 +53,33 @@ public class EvilEyeScript : MonoBehaviour {
 		}
 	}
 
+	public bool GetIfDead(){
+		return eyeLid.isDead;
+	}
+
 	void FireEyeBall(){
-		Debug.Log("Die, player!");
+		//Debug.Log("Die, player!");
 		return;
 	}
+
+	public void HitEye(PlayerMeleeScript meleeScript) {
+		//Debug.Log("I've Been Hit!");
+		int damage = meleeScript.GetCurrentAttackPower();
+		eyeHealth -= damage;
+		//FlashSprite(12, 0.1f);
+	}
+
+	IEnumerator FlashSprite(int numTimes, float delay) {
+        // number of times to loop
+        for (int loop = 0; loop < numTimes; loop++) {            
+            
+			spriteFlash.enabled = true;
+            // delay specified amount
+            yield return new WaitForSeconds(delay);
+ 
+            spriteFlash.enabled = false;
+            // delay specified amount
+            yield return new WaitForSeconds(delay);
+        }
+    }
 }
