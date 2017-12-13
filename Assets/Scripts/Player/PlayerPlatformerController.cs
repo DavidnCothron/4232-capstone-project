8deb8b707e0fbd27f2b2225df4914d612be03d59
Unknown <davidncothron@gmail.com>
2017-12-12 23:57:44 -0600

    Added phase, phase particle system, key pickup jingle, and more scenery

--- Assets/Scripts/Player/PlayerPlatformerController.cs
+++ Assets/Scripts/Player/PlayerPlatformerController.cs
@@ -9,15 +9,28 @@ public class PlayerPlatformerController : PhysicsObject {
 	public bool haltInput = false;
 	private int direction;
 	private bool attacking;
-	
+	private bool phasing;
+	private Vector2 phaseDirection;
+	private float phaseTime;
+	public float phaseUpModifier = 1f;
+	public float phaseTimeMax;
+	public float phaseSpeed;
 	[SerializeField]private SpriteRenderer spriteRenderer;
+	public GameObject phaseParticleSystem;
+	public ParticleSystem particleSystem;
 	[SerializeField]private Animator animator;
 	[SerializeField]private PlayerMeleeScript playerMelee;
+	[SerializeField]private PlayerHealthAndMagicController playerHealthAndMagicController;
 
 	// Use this for initialization
 	void Awake () {
 		//spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
 		//animator = gameObject.GetComponent<Animator> ();
+		if(playerHealthAndMagicController == null){
+			gameObject.GetComponent<PlayerHealthAndMagicController>();
+		}
+		phaseTime = phaseTimeMax;
+		phaseParticleSystem.SetActive(false);
 	}
 
 	public int getBufferListCount() {
@@ -48,11 +61,32 @@ public class PlayerPlatformerController : PhysicsObject {
 		return attacking;
 	}
 
+	private void Phase(){
+		switch(playerMelee.GetAttackDirection()){
+			case "right":
+				phaseDirection = new Vector2(1f, 0f);
+				break;
+			case "left":
+				phaseDirection = new Vector2(-1f, 0f);
+				break;
+			case "up":
+				phaseDirection = new Vector2(0f, 1f);
+				phaseTime *= phaseUpModifier; //This keeps upward movement feeling similar to lateral movement
+				break;
+			default:
+				break;
+		}
+
+		spriteRenderer.enabled = false;
+		phasing = true;
+		velocityUncapped = true;
+	}
+
 	protected override void ComputeVelocity()
 	{
 		Vector2 move = Vector2.zero;
 		
-		if (!haltInput) //Stop input if we need to halt it
+		if (!haltInput && !phasing) //Stop input if we need to halt it, also stop input if we are phasing
 		{
 			//Possibly temporarily halt input when attacking? (no direction switch?)
 			move.x = Input.GetAxis ("Horizontal"); //Get left or right input
@@ -71,7 +105,8 @@ public class PlayerPlatformerController : PhysicsObject {
 			}
 
 			if(Input.GetMouseButtonDown(1)){
-				
+				gravityModifier = 0f;
+				Phase();
 			}
 			
 			bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
@@ -88,6 +123,10 @@ public class PlayerPlatformerController : PhysicsObject {
 					direction = 1; //When 1, player is facing right
 					spriteRenderer.flipX = false;
 				}
+			}
+
+			if(Input.GetMouseButtonDown(1)){
+				Phase();
 			}
 
 			//Handle animation state logic here
@@ -109,6 +148,24 @@ public class PlayerPlatformerController : PhysicsObject {
 
 			//Apply target velocity here, which is utilized in the physics base
 			targetVelocity = move * maxSpeed;
+		}
+		else if(phasing){
+			phaseTime -= Time.deltaTime;
+			if(phaseTime >= 0){
+				targetVelocity = phaseDirection * phaseSpeed;
+				if(!phaseParticleSystem.active) phaseParticleSystem.SetActive(true);
+				particleSystem.Play();
+				velocity.y = targetVelocity.y;
+			}
+			else{
+				phasing = false;
+				velocityUncapped = false;
+				phaseTime = phaseTimeMax;
+				gravityModifier = 4;
+				particleSystem.Stop();
+				phaseParticleSystem.SetActive(false);
+				spriteRenderer.enabled = true;
+			}
 		}
 	}
 }
